2ae020f0deb82c06b2b0b4442c7708a162d89b13
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2024-10-24 08:55:36 +0200

    update

---
+++ sources/javascript/1/src/app.js
@@ -0,0 +1,23 @@
+let variabilerettangolo = document.querySelector("#rettangolo");
+
+//buttons
+let btnZoomIn = document.querySelector("#zoom-in");
+let btnChangeColor = document.querySelector("#change-color");
+let btnBorderRadius = document.querySelector("#border-radius");
+let btnMarginTop = document.querySelector("#margin-top");
+
+btnChangeColor.addEventListener("click", () => {
+  variabilerettangolo.classList.toggle("blue");
+});
+
+btnBorderRadius.addEventListener("dblclick", () => {
+  variabilerettangolo.classList.toggle("border");
+});
+
+btnMarginTop.addEventListener("mouseenter", () => {
+  variabilerettangolo.classList.add("marginTop");
+});
+
+btnMarginTop.addEventListener("mouseleave", () => {
+  variabilerettangolo.classList.remove("marginTop");
+});
